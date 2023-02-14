2ece3716eeb28f896b3b4a192482dbedbd8e4b59
jmg04006 <jmg04006@cub.uca.edu>
2023-02-14 13:05:43 -0600

    1st

---
+++ p1pkg/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'p1pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robotics-f',
+    maintainer_email='jmg04006@cub.uca.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
